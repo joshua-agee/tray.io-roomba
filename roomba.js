3949328ac337b957fe55fea995ad83b966b3bf20
Joshua Agee <joshua.agee@protonmail.com>
2021-09-30 12:28:04 -0500

    setting up variable extraction

--- roomba.js
+++ roomba.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+//define variables
+let boundaries = []; //size of our space
+let position = []; // roomba position
+let dirt = []; // list of dirt positions
+let path = []; //path taken by roomba
+
+
+//load input file
+var fs = require('fs');
+const data = fs.readFileSync('./input.txt', 'utf-8');
+// console.log(data);
+
+//parse data
+const lines = data.split(/\r?\n/)
+for(let i =0; i< lines.length; i++){
+    if (i == 0){
+        boundaries.push(lines[i].split(" "));
+    }
+    if (i == 1){
+        position.push(lines[i].split(" "));
+    }
+    if (i < lines.length -1){
+        dirt.push([lines[i].split(" ")]);
+    }
+    let path_str = lines[i];
+    path = path_str.split("");
+}
+console.log(`Boundaries: ${boundaries} \nPosition: ${position}\nDirt: ${dirt}\n Path: ${path}`);
+console.log(dirt[0][0]);
