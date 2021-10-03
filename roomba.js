c48b7a0247e36434fd4fcc4ae20b69ebc71bd22c
Joshua Agee <joshua.agee@protonmail.com>
2021-10-03 13:14:49 -0500

    one map per step

--- roomba.js
+++ roomba.js
@@ -5,25 +5,23 @@ let dirt = []; // list of dirt positions
 let path = []; //path taken by roomba
 let dirtCollected = 0;
 let lastPosition = [];
-const tileIcon = "🔲";
+let map = [];
+const tileIcon = " ";
 const dirtIcon = "💩";
 const cleanIcon = "✨";
-const northIcon = "⬆️";
-const southIcon = "⬇️";
-const westIcon = "⬅️";
-const eastIcon = "⬅️";
+const northIcon = "🔼";
+const southIcon = "🔽";
+const westIcon = "◀️";
+const eastIcon = "▶️";
 const startIcon = "⭕️";
 const endIcon = "❌";
 
-let map = [];
 
-const { Console } = require('console');
 //load input file
 var fs = require('fs');
 const data = fs.readFileSync('./input.txt', 'utf-8');
-// console.log(data);
 
-//parse data
+//parse initial data
 const lines = data.split(/\r?\n/)
 for(let i =0; i< lines.length; i++){
     if (i == 0){
@@ -44,21 +42,24 @@ for(let i =0; i< lines.length; i++){
 function addToMap(map, position, icon){
     position = position;
     icon = icon;
-    map.push({position, icon});
+    //unshift to ensure most recent is retrieved
+    map.unshift({position, icon});
 }
 
-
-// map.push({position, startIcon});
+//add dirt to map
+for(let i = 0; i<dirt.length; i++){
+    let dirtSpot = dirt[i];
+    addToMap(map, dirtSpot, dirtIcon);
+}
+//initial position
 addToMap(map, position, startIcon);
 
-// console.log(map[0]);
-// console.log(`Boundaries: ${boundaries} \nPosition: ${position}\nDirt: ${dirt}\n Path: ${path}`);
-// console.log(boundaries);
-// console.log(position);
-// console.log(dirt);
-// console.log(dirt[1][0]===position[0]&&dirt[1][1] === position[1]);
-// console.log(path);
 
+
+//starting map
+console.log("Starting position:")
+// console.log(map);
+console.log(makeMap());
 // process path and collect dirt
 //position = [X, Y]
 function move(currentPosition, direction, boundaries){
@@ -67,51 +68,38 @@ function move(currentPosition, direction, boundaries){
         case 'N': //y+1
             if ((currentPosition[1] + 1 ) > boundaries[1]){
                 newPosition = [currentPosition[0], boundaries[1]];
-                // map.push({newPosition, northIcon});
-                // addToMap(map, newPosition, northIcon);
                 // console.log('hit north wall');
             } else {
             newPosition = [currentPosition[0], currentPosition[1]+1];
-            // map.push({newPosition, northIcon});
-            // addToMap(map, newPosition, northIcon);
             }
-            addToMap(map, newPosition, northIcon);
+            addToMap(map, currentPosition, northIcon);
             break;
         case 'S': //y-1
             if ((currentPosition[1] - 1) < 0){
                 newPosition = [currentPosition[0], 0];
-                // map.push({newPosition, southIcon});
-                // addToMap(map, newPosition, southIcon);
                 // console.log('hit south wall');
             } else {
             newPosition = [currentPosition[0], currentPosition[1]-1];
-            // map.push({newPosition, southIcon});
             }
-            addToMap(map, newPosition, southIcon);
+            addToMap(map, currentPosition, southIcon);
             break;
         case 'W': //x-1
             if ((currentPosition[0] - 1) < 0){
                 newPosition = [0, currentPosition[1]];
-                // map.push({newPosition, westIcon});
-                // addToMap(map, newPosition, westIcon);
                 // console.log('hit west wall');
             } else {
             newPosition = [currentPosition[0]-1, currentPosition[1]];
-            // map.push({newPosition, westIcon});
             }
-            addToMap(map, newPosition, westIcon);
+            addToMap(map, currentPosition, westIcon);
             break;
         case 'E': //x+1
             if ((currentPosition[0] + 1 )> boundaries[0]){
                 newPosition = [boundaries[0], currentPosition[1]];
-                // map.push({newPosition, eastIcon});
-                // addToMap(map, newPosition, eastIcon);
                 // console.log('hit east wall');
             } else {
             newPosition = [currentPosition[0]+1, currentPosition[1]];
-            // map.push({newPosition, eastIcon});
             }
-            addToMap(map, newPosition, eastIcon);
+            addToMap(map, currentPosition, eastIcon);
             break;
     };
     return newPosition;
@@ -122,40 +110,34 @@ function pickupDirt(currentPosition, dirt){
         if(dirt[i][0]===currentPosition[0]&&dirt[i][1]===currentPosition[1]){
             // console.log('Yum!');
             dirtCollected++;
-            // map.push({currentPosition, cleanIcon});
             addToMap(map, currentPosition, cleanIcon);
             dirt.splice(i,1);
         }
     }
 }
 
-//initial position check for dirt
 //pickupDirt(position, dirt);
 //loop over path, update position, collect dirt, return final position
-// console.log('Starting position:');
-// console.log(position);
-// console.log('Moving!');
-path.forEach(direction => {
+
+path.forEach((direction, index) => {
     let newPosition = move(position, direction, boundaries);
     // console.log(newPosition);
     pickupDirt(newPosition, dirt);
     lastPosition = newPosition;
     position = newPosition;
+    console.log(`Step: ${index}:`);
+    console.log(makeMap());
 });
 
 // map.push({lastPosition, endIcon});
 addToMap(map, lastPosition, endIcon);
 let finalPosition = lastPosition[0].toString().concat(' ', lastPosition[1].toString());
 // console.log('results:');
-
+console.log("Results:");
 console.log(finalPosition);
 console.log(dirtCollected);
+console.log("Final Position:");
 
-for(let i = 0; i<dirt.length; i++){
-    let dirtSpot = dirt[i];
-    // map.push({dirtSpot, dirtIcon});
-    addToMap(map, dirtSpot, dirtIcon);
-}
 //build map
 // console.log(map);
 // console.log(map.length);
@@ -169,21 +151,24 @@ function getIconFromMap(map, x, y){
 // console.log(getIconFromMap(map, 4,2));
 
 //build map from results
-let mapResult = [];
-for(let y=0; y<=boundaries[1]; y++){
-    let row=[];
-    for(let x=0; x<= boundaries[0]; x++){
-        try {
-            // console.log(getIconFromMap(map, x, y));
-            row.push(getIconFromMap(map, x, y));
+function makeMap(){
+    let mapResult = [];
+    for(let y=0; y<=boundaries[1]; y++){
+        let row=[];
+        for(let x=0; x<= boundaries[0]; x++){
+            try {
+                // console.log(getIconFromMap(map, x, y));
+                row.push(getIconFromMap(map, x, y));
+            }
+            catch(error) {
+                // console.log(error);
+                row.push(tileIcon);
+            }
         }
-        catch(error) {
-            // console.log(error);
-            row.push(tileIcon);
-        }
+        mapResult.unshift(row.toString());
+        row = [];
     }
-    mapResult.unshift(row.toString());
-    row = [];
+    return mapResult;
 }
 
-console.log(mapResult);
+console.log(makeMap());
